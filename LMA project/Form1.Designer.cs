535afd43de268b9453d64c3f21fc0df37dcf7a1c
Omar Aboslo <I455209@fhict.nl>
2020-11-25 09:44:31 +0100

    1

--- LMA project/Form1.Designer.cs
+++ LMA project/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkBox1.Location = new System.Drawing.Point(333, 185);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 0;
