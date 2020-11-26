6fda7deeffa9d6b3e48bb89f91b0da9de8992cda
ammar-gif <73993769+ammar-gif@users.noreply.github.com>
2020-11-26 15:33:03 +0100

    Laatste versie voor vandaag

---
+++ LMA project/Form4.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace LMA_project
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonhooikoorts = new System.Windows.Forms.Button();
+            this.buttondepressie = new System.Windows.Forms.Button();
+            this.buttonAlzheimer = new System.Windows.Forms.Button();
+            this.buttonKeelontsteking = new System.Windows.Forms.Button();
+            this.buttonAids = new System.Windows.Forms.Button();
+            this.buttonAdhd = new System.Windows.Forms.Button();
+            this.buttonGriep = new System.Windows.Forms.Button();
+            this.buttonAstma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonhooikoorts
+            // 
+            this.buttonhooikoorts.Location = new System.Drawing.Point(438, 334);
+            this.buttonhooikoorts.Name = "buttonhooikoorts";
+            this.buttonhooikoorts.Size = new System.Drawing.Size(344, 36);
+            this.buttonhooikoorts.TabIndex = 15;
+            this.buttonhooikoorts.Text = "hooikoorts";
+            this.buttonhooikoorts.UseVisualStyleBackColor = true;
+            this.buttonhooikoorts.Click += new System.EventHandler(this.buttonhooikoorts_Click);
+            // 
+            // buttondepressie
+            // 
+            this.buttondepressie.Location = new System.Drawing.Point(438, 245);
+            this.buttondepressie.Name = "buttondepressie";
+            this.buttondepressie.Size = new System.Drawing.Size(344, 36);
+            this.buttondepressie.TabIndex = 14;
+            this.buttondepressie.Text = "Depressie";
+            this.buttondepressie.UseVisualStyleBackColor = true;
+            this.buttondepressie.Click += new System.EventHandler(this.buttondepressie_Click);
+            // 
+            // buttonAlzheimer
+            // 
+            this.buttonAlzheimer.Location = new System.Drawing.Point(438, 171);
+            this.buttonAlzheimer.Name = "buttonAlzheimer";
+            this.buttonAlzheimer.Size = new System.Drawing.Size(344, 36);
+            this.buttonAlzheimer.TabIndex = 13;
+            this.buttonAlzheimer.Text = "Alzheimer";
+            this.buttonAlzheimer.UseVisualStyleBackColor = true;
+            this.buttonAlzheimer.Click += new System.EventHandler(this.buttonAlzheimer_Click);
+            // 
+            // buttonKeelontsteking
+            // 
+            this.buttonKeelontsteking.Location = new System.Drawing.Point(438, 81);
+            this.buttonKeelontsteking.Name = "buttonKeelontsteking";
+            this.buttonKeelontsteking.Size = new System.Drawing.Size(344, 36);
+            this.buttonKeelontsteking.TabIndex = 12;
+            this.buttonKeelontsteking.Text = "keelontsteking";
+            this.buttonKeelontsteking.UseVisualStyleBackColor = true;
+            this.buttonKeelontsteking.Click += new System.EventHandler(this.buttonKeelontsteking_Click);
+            // 
+            // buttonAids
+            // 
+            this.buttonAids.Location = new System.Drawing.Point(19, 334);
+            this.buttonAids.Name = "buttonAids";
+            this.buttonAids.Size = new System.Drawing.Size(344, 36);
+            this.buttonAids.TabIndex = 11;
+            this.buttonAids.Text = "Aids";
+            this.buttonAids.UseVisualStyleBackColor = true;
+            this.buttonAids.Click += new System.EventHandler(this.buttonAids_Click);
+            // 
+            // buttonAdhd
+            // 
+            this.buttonAdhd.Location = new System.Drawing.Point(19, 245);
+            this.buttonAdhd.Name = "buttonAdhd";
+            this.buttonAdhd.Size = new System.Drawing.Size(344, 36);
+            this.buttonAdhd.TabIndex = 10;
+            this.buttonAdhd.Text = "ADHD";
+            this.buttonAdhd.UseVisualStyleBackColor = true;
+            this.buttonAdhd.Click += new System.EventHandler(this.buttonAdhd_Click);
+            // 
+            // buttonGriep
+            // 
+            this.buttonGriep.Location = new System.Drawing.Point(19, 171);
+            this.buttonGriep.Name = "buttonGriep";
+            this.buttonGriep.Size = new System.Drawing.Size(344, 36);
+            this.buttonGriep.TabIndex = 9;
+            this.buttonGriep.Text = "Griep";
+            this.buttonGriep.UseVisualStyleBackColor = true;
+            this.buttonGriep.Click += new System.EventHandler(this.buttonGriep_Click);
+            // 
+            // buttonAstma
+            // 
+            this.buttonAstma.Location = new System.Drawing.Point(19, 81);
+            this.buttonAstma.Name = "buttonAstma";
+            this.buttonAstma.Size = new System.Drawing.Size(344, 36);
+            this.buttonAstma.TabIndex = 8;
+            this.buttonAstma.Text = "Astma";
+            this.buttonAstma.UseVisualStyleBackColor = true;
+            this.buttonAstma.Click += new System.EventHandler(this.buttonAstma_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonhooikoorts);
+            this.Controls.Add(this.buttondepressie);
+            this.Controls.Add(this.buttonAlzheimer);
+            this.Controls.Add(this.buttonKeelontsteking);
+            this.Controls.Add(this.buttonAids);
+            this.Controls.Add(this.buttonAdhd);
+            this.Controls.Add(this.buttonGriep);
+            this.Controls.Add(this.buttonAstma);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonhooikoorts;
+        private System.Windows.Forms.Button buttondepressie;
+        private System.Windows.Forms.Button buttonAlzheimer;
+        private System.Windows.Forms.Button buttonKeelontsteking;
+        private System.Windows.Forms.Button buttonAids;
+        private System.Windows.Forms.Button buttonAdhd;
+        private System.Windows.Forms.Button buttonGriep;
+        private System.Windows.Forms.Button buttonAstma;
+    }
+}
