fc658b43855d9f66d93cf47b7c2d5dbe36cc9e41
ammar-gif <73993769+ammar-gif@users.noreply.github.com>
2020-12-01 09:35:00 +0100

    omar

--- LMA project/Hallo.Designer.cs
+++ LMA project/Hallo.Designer.cs
@@ -45,9 +45,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(595, 126);
+            this.label1.Location = new System.Drawing.Point(793, 155);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 33);
+            this.label1.Size = new System.Drawing.Size(131, 41);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welkom";
             // 
@@ -55,9 +56,10 @@
             // 
             this.Button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.Button1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Button1.Location = new System.Drawing.Point(484, 240);
+            this.Button1.Location = new System.Drawing.Point(645, 292);
+            this.Button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Button1.Name = "Button1";
-            this.Button1.Size = new System.Drawing.Size(312, 49);
+            this.Button1.Size = new System.Drawing.Size(416, 60);
             this.Button1.TabIndex = 1;
             this.Button1.Text = "Ik heb symptomen";
             this.Button1.UseVisualStyleBackColor = false;
@@ -67,9 +69,10 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.button2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(484, 339);
+            this.button2.Location = new System.Drawing.Point(645, 417);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(312, 49);
+            this.button2.Size = new System.Drawing.Size(416, 60);
             this.button2.TabIndex = 2;
             this.button2.Text = "Ik wil een bepaalde ziekte zoekn";
             this.button2.UseVisualStyleBackColor = false;
@@ -78,9 +81,10 @@
             // 
             this.button3.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.button3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(484, 443);
+            this.button3.Location = new System.Drawing.Point(645, 545);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(312, 49);
+            this.button3.Size = new System.Drawing.Size(416, 60);
             this.button3.TabIndex = 3;
             this.button3.Text = "Gezond lifstyle";
             this.button3.UseVisualStyleBackColor = false;
@@ -89,9 +93,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(553, 174);
+            this.label2.Location = new System.Drawing.Point(737, 214);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(200, 33);
+            this.label2.Size = new System.Drawing.Size(254, 41);
             this.label2.TabIndex = 4;
             this.label2.Text = "Wat wilt u doen?";
             // 
@@ -101,8 +106,9 @@
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(266, 563);
+            this.panel1.Size = new System.Drawing.Size(355, 693);
             this.panel1.TabIndex = 5;
             // 
             // label4
@@ -110,9 +116,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.label4.Location = new System.Drawing.Point(19, 301);
+            this.label4.Location = new System.Drawing.Point(25, 370);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(247, 39);
+            this.label4.Size = new System.Drawing.Size(307, 49);
             this.label4.TabIndex = 7;
             this.label4.Text = "Health is treasure";
             // 
@@ -121,28 +128,30 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.label3.Location = new System.Drawing.Point(-9, 61);
+            this.label3.Location = new System.Drawing.Point(-12, 75);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(284, 146);
+            this.label3.Size = new System.Drawing.Size(355, 183);
             this.label3.TabIndex = 6;
             this.label3.Text = "LMA";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::LMA_project.Properties.Resources.soso;
-            this.pictureBox1.Location = new System.Drawing.Point(401, -19);
+            this.pictureBox1.Location = new System.Drawing.Point(535, -23);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(460, 150);
+            this.pictureBox1.Size = new System.Drawing.Size(613, 185);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Hallo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.ClientSize = new System.Drawing.Size(1312, 690);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
@@ -150,6 +159,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.Button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Hallo";
             this.Text = "Hallo";
             this.panel1.ResumeLayout(false);
