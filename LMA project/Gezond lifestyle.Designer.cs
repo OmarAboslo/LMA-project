522ddac511869aa1ed5603e61dcc53f3d6180953
ammar-gif <73993769+ammar-gif@users.noreply.github.com>
2020-12-08 09:31:14 +0100

    Ammar gezond lifestyle

--- LMA project/Gezond lifestyle.Designer.cs
+++ LMA project/Gezond lifestyle.Designer.cs
@@ -36,6 +36,15 @@
             this.textbGeboortedatm = new System.Windows.Forms.TextBox();
             this.butOk = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxGewensteGewicht = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.RBNormaal = new System.Windows.Forms.RadioButton();
+            this.RBSnel = new System.Windows.Forms.RadioButton();
+            this.RBZosnelmogelijk = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RBJa = new System.Windows.Forms.RadioButton();
+            this.RBNee = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,9 +52,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Andalus", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(150, 168);
+            this.label1.Location = new System.Drawing.Point(35, 91);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 52);
+            this.label1.Size = new System.Drawing.Size(150, 50);
             this.label1.TabIndex = 0;
             this.label1.Text = "Gewicht:";
             this.label1.Click += new System.EventHandler(this.label1_Click_1);
@@ -54,9 +63,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Andalus", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(163, 231);
+            this.label2.Location = new System.Drawing.Point(421, 91);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(129, 52);
+            this.label2.Size = new System.Drawing.Size(124, 50);
             this.label2.TabIndex = 1;
             this.label2.Text = "Lengte:";
             // 
@@ -64,16 +73,16 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Andalus", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(120, 294);
+            this.label3.Location = new System.Drawing.Point(244, 153);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(261, 52);
+            this.label3.Size = new System.Drawing.Size(136, 50);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Geboortedatum:";
+            this.label3.Text = "Leeftijd:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // TextbGewicht
             // 
-            this.TextbGewicht.Location = new System.Drawing.Point(456, 181);
+            this.TextbGewicht.Location = new System.Drawing.Point(191, 101);
             this.TextbGewicht.Multiline = true;
             this.TextbGewicht.Name = "TextbGewicht";
             this.TextbGewicht.Size = new System.Drawing.Size(158, 31);
@@ -81,7 +90,7 @@
             // 
             // TextbLengte
             // 
-            this.TextbLengte.Location = new System.Drawing.Point(456, 244);
+            this.TextbLengte.Location = new System.Drawing.Point(568, 101);
             this.TextbLengte.Multiline = true;
             this.TextbLengte.Name = "TextbLengte";
             this.TextbLengte.Size = new System.Drawing.Size(158, 31);
@@ -89,7 +98,7 @@
             // 
             // textbGeboortedatm
             // 
-            this.textbGeboortedatm.Location = new System.Drawing.Point(456, 314);
+            this.textbGeboortedatm.Location = new System.Drawing.Point(387, 163);
             this.textbGeboortedatm.Multiline = true;
             this.textbGeboortedatm.Name = "textbGeboortedatm";
             this.textbGeboortedatm.Size = new System.Drawing.Size(158, 31);
@@ -115,12 +124,114 @@
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
+            // textBoxGewensteGewicht
+            // 
+            this.textBoxGewensteGewicht.Location = new System.Drawing.Point(553, 223);
+            this.textBoxGewensteGewicht.Multiline = true;
+            this.textBoxGewensteGewicht.Name = "textBoxGewensteGewicht";
+            this.textBoxGewensteGewicht.Size = new System.Drawing.Size(164, 38);
+            this.textBoxGewensteGewicht.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(85, 279);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(415, 29);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Hoe snel wil je aankomen of afvallen?";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(85, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(329, 29);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Wat is het gewenste gewicht?";
+            // 
+            // RBNormaal
+            // 
+            this.RBNormaal.AutoSize = true;
+            this.RBNormaal.Location = new System.Drawing.Point(132, 351);
+            this.RBNormaal.Name = "RBNormaal";
+            this.RBNormaal.Size = new System.Drawing.Size(82, 21);
+            this.RBNormaal.TabIndex = 18;
+            this.RBNormaal.TabStop = true;
+            this.RBNormaal.Text = "Normaal";
+            this.RBNormaal.UseVisualStyleBackColor = true;
+            // 
+            // RBSnel
+            // 
+            this.RBSnel.AutoSize = true;
+            this.RBSnel.Location = new System.Drawing.Point(373, 351);
+            this.RBSnel.Name = "RBSnel";
+            this.RBSnel.Size = new System.Drawing.Size(57, 21);
+            this.RBSnel.TabIndex = 19;
+            this.RBSnel.TabStop = true;
+            this.RBSnel.Text = "Snel";
+            this.RBSnel.UseVisualStyleBackColor = true;
+            // 
+            // RBZosnelmogelijk
+            // 
+            this.RBZosnelmogelijk.AutoSize = true;
+            this.RBZosnelmogelijk.Location = new System.Drawing.Point(611, 362);
+            this.RBZosnelmogelijk.Name = "RBZosnelmogelijk";
+            this.RBZosnelmogelijk.Size = new System.Drawing.Size(131, 21);
+            this.RBZosnelmogelijk.TabIndex = 20;
+            this.RBZosnelmogelijk.TabStop = true;
+            this.RBZosnelmogelijk.Text = "Zo snel mogelijk";
+            this.RBZosnelmogelijk.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(85, 408);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(632, 29);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Heb je een ziekte of ben je allergisch voor iets specifieks?";
+            // 
+            // RBJa
+            // 
+            this.RBJa.AutoSize = true;
+            this.RBJa.Location = new System.Drawing.Point(232, 466);
+            this.RBJa.Name = "RBJa";
+            this.RBJa.Size = new System.Drawing.Size(44, 21);
+            this.RBJa.TabIndex = 22;
+            this.RBJa.TabStop = true;
+            this.RBJa.Text = "Ja";
+            this.RBJa.UseVisualStyleBackColor = true;
+            // 
+            // RBNee
+            // 
+            this.RBNee.AutoSize = true;
+            this.RBNee.Location = new System.Drawing.Point(475, 466);
+            this.RBNee.Name = "RBNee";
+            this.RBNee.Size = new System.Drawing.Size(55, 21);
+            this.RBNee.TabIndex = 23;
+            this.RBNee.TabStop = true;
+            this.RBNee.Text = "Nee";
+            this.RBNee.UseVisualStyleBackColor = true;
+            // 
             // Sport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(813, 556);
+            this.Controls.Add(this.RBNee);
+            this.Controls.Add(this.RBJa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.RBZosnelmogelijk);
+            this.Controls.Add(this.RBSnel);
+            this.Controls.Add(this.RBNormaal);
+            this.Controls.Add(this.textBoxGewensteGewicht);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.butOk);
             this.Controls.Add(this.textbGeboortedatm);
@@ -148,5 +259,14 @@
         private System.Windows.Forms.TextBox textbGeboortedatm;
         private System.Windows.Forms.Button butOk;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxGewensteGewicht;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton RBNormaal;
+        private System.Windows.Forms.RadioButton RBSnel;
+        private System.Windows.Forms.RadioButton RBZosnelmogelijk;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton RBJa;
+        private System.Windows.Forms.RadioButton RBNee;
     }
 }
