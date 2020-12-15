66ea924c50fa24baf848860cd84d36c56c5e176e
Omar Aboslo <I455209@fhict.nl>
2020-12-15 11:32:45 +0100

    class dieet

---
+++ LMA project/ClassDieet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMA_project
+{
+    class ClassDieet
+    {
+        private  enum Vlees {Lam = 300, kip = 250, vis = 219, kalf = 123 }
+        private enum Groente{sla, komkommer, tomaat, aardappel }
+        private enum Fruit {banaan, appel, druif, aardbei }
+        private enum Dranken {water, melk, fruitsap, koffie }
+
+        public ClassDieet(int eenheid = 100)
+        {
+          
+
+
+         
+        }
+            
+
+
+
+    }
+}
