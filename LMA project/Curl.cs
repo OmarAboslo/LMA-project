215d5ce6812ba1db4b6f97238ea7733308f59034
ammar-gif <73993769+ammar-gif@users.noreply.github.com>
2021-01-08 09:38:34 +0100

    f

---
+++ LMA project/Curl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+namespace LMA_project
+{
+    public partial class Curl : Form
+    {
+        public Curl()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
